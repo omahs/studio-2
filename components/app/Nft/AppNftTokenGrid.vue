20b787bac92db6b0ab92144b11953df1296161cf
angelorc <angelo@rcstart.it>
2024-04-29 19:07:34 +0200

    feat: add nft transfer

--- components/app/Nft/AppNftTokenGrid.vue
+++ components/app/Nft/AppNftTokenGrid.vue
@@ -9,7 +9,8 @@
     </v-row>
     <v-row>
       <v-col v-for="{ nft, nftName, tokenId, tokenName, image } in items" :key="`${nft}:${tokenId}`" md="3" cols="12">
-        <AppNftTokenGridItem :nft="nft" :nft-name="nftName" :image="image" :token-name="tokenName" :token-id="tokenId" />
+        <AppNftTokenGridItem :nft="nft" :nft-name="nftName" :image="image" :token-name="tokenName" :token-id="tokenId"
+          @refresh="onRefresh" />
       </v-col>
     </v-row>
   </v-container>
@@ -22,4 +23,12 @@ defineProps<{
   title?: string | null;
   items?: NftTokenGridItem[] | null;
 }>();
+
+const emit = defineEmits<{
+  (e: "refresh"): void;
+}>();
+
+function onRefresh() {
+  emit("refresh");
+}
 </script>
--- pages/me/index.vue
+++ pages/me/index.vue
@@ -1,11 +1,10 @@
 <template>
   <app-page>
     <template #body>
-      <AppProfileHeader
-v-if="user" :address="user.address" :avatar="user.avatar" :cover="user.cover"
+      <AppProfileHeader v-if="user" :address="user.address" :avatar="user.avatar" :cover="user.cover"
         :username="user.username" :email="user.email" />
 
-      <AppNftTokenGrid class="mt-4" :items="data" />
+      <AppNftTokenGrid class="mt-4" :items="data" @refresh="onRefresh" />
     </template>
   </app-page>
 </template>
@@ -27,7 +26,7 @@ useSeoMeta({
   ogImage: user?.value?.avatar ? useIpfsLink(user?.value?.avatar) : defaultImage,
 });
 
-const { data, error } = useFetch(`/api/u/${user.value?.address}/nfts`, {
+const { data, error, refresh } = useFetch(`/api/u/${user.value?.address}/nfts`, {
   transform: (data) => data.map((item) => {
     return {
       nft: item.nft_id,
@@ -38,6 +37,10 @@ const { data, error } = useFetch(`/api/u/${user.value?.address}/nfts`, {
     } as NftTokenGridItem
   })
 })
+
+async function onRefresh() {
+  await refresh()
+}
 
 if (error.value) {
   navigateTo("/")
