b55db09e2c9177c7373eddb440f5cfd3f23ca35d
angelorc <angelo@rcstart.it>
2024-05-03 23:44:45 +0200

    wip: player queue

--- components/app/Player/AppPlayerQueue.client.vue
+++ components/app/Player/AppPlayerQueue.client.vue
@@ -1,29 +1,39 @@
 <template>
-  <v-card class="player-queue" v-if="showQueue" rounded="xl">
-    <v-card flat rounded>
-      <v-card-title class="d-flex justify-space-between align-center pr-0">
-        Queue
-        <v-btn icon="mdi-close" variant="text" color="white" @click="toggleQueue" />
-      </v-card-title>
+  <v-responsive class="player-queue" v-if="showQueue" rounded="xl">
+
+    <v-card v-if="track" flat class="pa-4 pt-6">
+      <v-row class="d-flex" align="center" justify="space-around" no-gutters>
+        <v-col cols="auto">
+          <NuxtImg :src="track.cover" width="287" fit="cover" class="rounded-xl" />
+        </v-col>
+      </v-row>
+      <v-row class="d-flex mt-0" align="center">
+        <v-col cols="auto">
+          <div class="text-h5">{{ track.title }}</div>
+          <div class="text-surface-variant text-h6">{{ track.artist }}</div>
+        </v-col>
+      </v-row>
     </v-card>
 
     <v-card v-if="track" flat>
-      <v-card-title class="d-flex justify-space-between align-center pr-0 text-subtitle-1">
-        Stai ascoltando
+      <v-card-title class="d-flex justify-space-between align-center pr-0 text-body-1 text-surface-variant">
+        Prossimi brani
       </v-card-title>
       <v-card-text>
-        <v-row>
-          <v-col cols="auto">
-            <NuxtImg :src="track?.cover" height="56" width="56" />
-          </v-col>
-          <v-col class="pl-3">
-            <div class="text-subtitle-1">{{ track.title }}</div>
-            <div class="text-surface-variant">{{ track.artist }}</div>
-          </v-col>
-        </v-row>
+        <v-responsive :max-height="300" style="overflow-y: auto;">
+          <v-row v-for="i in 20" no-gutters class="mb-2">
+            <v-col cols="auto">
+              <NuxtImg :src="track?.cover" width="56" fit="cover" class="rounded-md" />
+            </v-col>
+            <v-col class="pl-3">
+              <div class="text-subtitle-1">{{ track.title }}</div>
+              <div class="text-surface-variant">{{ track.artist }}</div>
+            </v-col>
+          </v-row>
+        </v-responsive>
       </v-card-text>
     </v-card>
-  </v-card>
+  </v-responsive>
 </template>
 
 <script lang="ts" setup>
