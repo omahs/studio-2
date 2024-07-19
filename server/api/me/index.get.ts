804415335f341cb70c7e76ed8641171e380036c2
angelorc <angelo@rcstart.it>
2024-07-19 22:58:40 +0200

    auth fix

--- server/api/me/index.get.ts
+++ server/api/me/index.get.ts
@@ -1,17 +1,18 @@
 export default defineEventHandler(async (event) => {
   const authRequest = auth.handleRequest(event);
   const session = await authRequest.validate();
-  if (!session) {
-    throw createError({
-      message: 'Unauthorized',
-      status: 401
-    });
-  }
+
+  // if (!session) {
+  //   throw createError({
+  //     message: 'Unauthorized',
+  //     status: 401
+  //   });
+  // }
 
   return {
     user: {
-      sid: session.sessionId,
-      ...session.user,
+      sid: session?.sessionId,
+      ...session?.user
     }
   };
 });
