fc69338b23c1157136586b99b8ebaf8cfae34fb5
angelorc <angelo@rcstart.it>
2024-07-22 13:50:08 +0200

    feat: improve profile page

--- pages/me/index.vue
+++ pages/me/index.vue
@@ -4,18 +4,73 @@
       <AppProfileHeader v-if="user" :address="user.address" :avatar="user.avatar" :cover="user.cover"
         :username="user.username" :email="user.email" />
 
-      <AppNftTokenGrid class="mt-4" :items="data" @refresh="onRefresh" />
+      <v-skeleton-loader v-if="isFetching" type="card" width="320" class="ml-4" />
+      <v-container v-if="nfts2">
+        <v-row>
+          <v-col cols="auto" v-for="nft in nfts2.nfts" :key="nft.nft">
+            <v-card width="316">
+              <v-card-text>
+                <NuxtLink :to="`/nfts/${nft.nft}`">
+                  <NuxtImg :src="useIpfsLink(nft.image)" width="285" aspect="1" />
+                </NuxtLink>
+                <div class="d-flex justify-space-between">
+                  <div class="text-h6" :style="{ height: '65px' }">
+                    <NuxtLink :to="`/nfts/${nft.nft}`" class="text-decoration-none text-white">
+                      {{ nft.name }}
+                    </NuxtLink>
+                  </div>
+                  <div v-if="showContextMenu">
+                    <v-menu location="bottom right">
+                      <template #activator="{ props }">
+                        <v-btn variant="text" color="white" icon="mdi-dots-vertical" v-bind="props" />
+                      </template>
+
+                      <v-list density="compact" :style="{ cursor: 'pointer' }">
+                        <v-list-item density="compact" append-icon="mdi-send" title="Transfer NFT" />
+                      </v-list>
+                    </v-menu>
+                  </div>
+                </div>
+                <div class="d-flex justify-space-between">
+                  <div class="text-subtitle-2 text-surface-variant" @click.stop="setTokens(nft.tokenIds)"
+                    :style="{ cursor: 'pointer' }">
+                    {{ nft.totalIds }} edition{{ nft.totalIds > 1 ? 's' : '' }}
+                  </div>
+                  <div class="text-subtitle-2 text-surface-variant">{{ formatNumber(nft.value) }} BTSG</div>
+                </div>
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+
+      <v-dialog v-model="showTokensDialog" width="360" height="400">
+        <v-card>
+          <v-card-title>Editions</v-card-title>
+          <v-card-text>
+            <v-chip size="small" rounded v-for="id in tokens" :key="id" class="mr-2 mb-2">
+              #{{ id }}
+            </v-chip>
+          </v-card-text>
+          <v-card-actions>
+            <v-btn @click.stop="closeTokensDialog">Close</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
     </template>
   </app-page>
 </template>
 
 <script setup lang="ts">
-import type { NftTokenGridItem } from '~/components/app/Nft/AppNftTokenGridItem.vue';
 import defaultImage from "~/assets/images/og-default-1200.png";
+import { useQuery } from '@tanstack/vue-query'
 
 definePageMeta({
   middleware: ["protected"]
 });
+
+const route = useRoute();
+const showContextMenu = computed(() => route.name === "me");
 
 const user = useUserState()
 useSeoMeta({
@@ -26,23 +81,41 @@ useSeoMeta({
   ogImage: user?.value?.avatar ? useIpfsLink(user?.value?.avatar) : defaultImage,
 });
 
-const { data, error, refresh } = useFetch(`/api/u/${user.value?.address}/nfts`, {
-  transform: (data) => data.map((item) => {
-    return {
-      nft: item.nft_id,
-      nftName: item.nft_name,
-      tokenId: item.token_id,
-      tokenName: `#${item.token_id}`,
-      image: item.nft_image,
-    } as NftTokenGridItem
-  })
-})
-
-async function onRefresh() {
-  await refresh()
+interface NftsResponse {
+  totalCount: number;
+  totalValue: number;
+  nfts: {
+    nft: string;
+    name: string;
+    image: string;
+    totalIds: number;
+    tokenIds: string[]
+    value: number;
+  }[]
 }
 
-if (error.value) {
-  navigateTo("/")
+const { isFetching, data: nfts2 } = useQuery({
+  queryKey: ['profile', user.value!.address, 'nfts'],
+  queryFn: async () => {
+    const response = await $fetch<NftsResponse>(`${useRuntimeConfig().public.mediaApiDirect}/u/${user.value!.address}/nfts`)
+
+    return {
+      totalCount: response.totalCount,
+      totalValue: response.totalValue,
+      nfts: response.nfts.sort((a, b) => b.value - a.value)
+    }
+  },
+  staleTime: 1000 * 60 * 5, // 5 minutes,
+})
+
+const tokens = ref([])
+const showTokensDialog = computed(() => tokens.value.length > 0)
+
+function setTokens(ids: string[]) {
+  tokens.value = ids.map(id => parseInt(id)).sort((a, b) => a - b).map(id => id.toString())
+}
+
+function closeTokensDialog() {
+  tokens.value = []
 }
 </script>
--- pages/u/[address].vue
+++ pages/u/[address].vue
@@ -1,23 +1,76 @@
 <template>
   <app-page>
     <template #body>
-      <AppProfileHeader
-:address="address" :avatar="user?.avatar" :cover="user?.cover" :username="user?.username"
+      <AppProfileHeader :address="address" :avatar="user?.avatar" :cover="user?.cover" :username="user?.username"
         :email="user?.email" />
 
       <!--<AppUpcomingDrops v-if="address === 'bitsong1f5ze3svwg8fgjuwwnr743j6fr9vtyr58nex7tu'" class="my-8" />-->
+      <v-skeleton-loader v-if="isFetching" type="card" width="320" class="ml-4" />
+      <v-container v-if="nfts2">
+        <v-row>
+          <v-col cols="auto" v-for="nft in nfts2.nfts" :key="nft.nft">
+            <v-card width="316">
+              <v-card-text>
+                <NuxtLink :to="`/nfts/${nft.nft}`">
+                  <NuxtImg :src="useIpfsLink(nft.image)" width="285" aspect="1" />
+                </NuxtLink>
+                <div class="d-flex justify-space-between">
+                  <div class="text-h6" :style="{ height: '65px' }">
+                    <NuxtLink :to="`/nfts/${nft.nft}`" class="text-decoration-none text-white">
+                      {{ nft.name }}
+                    </NuxtLink>
+                  </div>
+                  <div v-if="showContextMenu">
+                    <v-menu location="bottom right">
+                      <template #activator="{ props }">
+                        <v-btn variant="text" color="white" icon="mdi-dots-vertical" v-bind="props" />
+                      </template>
 
-      <AppNftTokenGrid class="my-8" title="Collected" :items="nfts" />
+                      <v-list density="compact" :style="{ cursor: 'pointer' }">
+                        <v-list-item density="compact" append-icon="mdi-send" title="Transfer NFT" />
+                      </v-list>
+                    </v-menu>
+                  </div>
+                </div>
+                <div class="d-flex justify-space-between">
+                  <div class="text-subtitle-2 text-surface-variant" @click.stop="setTokens(nft.tokenIds)"
+                    :style="{ cursor: 'pointer' }">
+                    {{ nft.totalIds }} edition{{ nft.totalIds > 1 ? 's' : '' }}
+                  </div>
+                  <div class="text-subtitle-2 text-surface-variant">{{ formatNumber(nft.value) }} BTSG</div>
+                </div>
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+
+      <v-dialog v-model="showTokensDialog" width="360" height="400">
+        <v-card>
+          <v-card-title>Editions</v-card-title>
+          <v-card-text>
+            <v-chip size="small" rounded v-for="id in tokens" :key="id" class="mr-2 mb-2">
+              #{{ id }}
+            </v-chip>
+          </v-card-text>
+          <v-card-actions>
+            <v-btn @click.stop="closeTokensDialog">Close</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
     </template>
   </app-page>
 </template>
 
 <script setup lang="ts">
-import type { NftTokenGridItem } from '~/components/app/Nft/AppNftTokenGridItem.vue';
 import defaultImage from "~/assets/images/og-default-1200.png";
 import { isValidAddress } from '@bitsongjs/metadata'
+import { useQuery } from '@tanstack/vue-query'
 
 const address = computed(() => useRoute().params.address as string)
+
+const route = useRoute();
+const showContextMenu = computed(() => route.name === "me");
 
 watch(address, (value) => {
   if (!isValidAddress(value, "bitsong")) {
@@ -36,19 +89,41 @@ useSeoMeta({
   ogImage: user?.value?.avatar ? useIpfsLink(user?.value?.avatar) : defaultImage,
 });
 
-// if (error.value) {
-//   navigateTo("/")
-// }
+interface NftsResponse {
+  totalCount: number;
+  totalValue: number;
+  nfts: {
+    nft: string;
+    name: string;
+    image: string;
+    totalIds: number;
+    tokenIds: string[]
+    value: number;
+  }[]
+}
 
-const { data: nfts } = useFetch(`/api/u/${address.value}/nfts`, {
-  transform: (data) => data.map((item) => {
+const { isFetching, data: nfts2 } = useQuery({
+  queryKey: ['profile', user.value!.address, 'nfts'],
+  queryFn: async () => {
+    const response = await $fetch<NftsResponse>(`${useRuntimeConfig().public.mediaApiDirect}/u/${user.value!.address}/nfts`)
+
     return {
-      nft: item.nft_id,
-      nftName: item.nft_name,
-      tokenId: item.token_id,
-      tokenName: `#${item.token_id}`,
-      image: item.nft_image,
-    } as NftTokenGridItem
-  })
+      totalCount: response.totalCount,
+      totalValue: response.totalValue,
+      nfts: response.nfts.sort((a, b) => b.value - a.value)
+    }
+  },
+  staleTime: 1000 * 60 * 5, // 5 minutes,
 })
+
+const tokens = ref([])
+const showTokensDialog = computed(() => tokens.value.length > 0)
+
+function setTokens(ids: string[]) {
+  tokens.value = ids.map(id => parseInt(id)).sort((a, b) => a - b).map(id => id.toString())
+}
+
+function closeTokensDialog() {
+  tokens.value = []
+}
 </script>
